798d4b5932c83e48487ab1a418891a7894eec0ad
Matej <matej.bernatovic@gmail.com>
2017-11-30 12:03:23 +0100

    Add 404.html , class for 404 error

--- server.py
+++ server.py
@@ -18,11 +18,22 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             elif os.path.isfile(full_path):
                 self.handle_file(full_path)
         except Exception as msg:
-            #self.handle_error(msg)
-            print msg
+            self.handle_error(msg)
+            
+    def handle_error(self , msg):
+        self.send_response(404)
+        self.send_header("Content-Type", "text/html")
+        page = self.read_file(os.getcwd()+"/404.html")
+        print page
+        page = page.replace("{msg_placeholder}", str(msg))
+        self.send_header("Content-Length", str(len(page))) 
+        self.end_headers()
+        self.wfile.write(page)
+
     def send_page(self, page):
         self.send_response(200)
-        self.send_header("Content-Type","text/html")       
+        self.send_header("Content-Type","text/html")    
+
         self.send_header("Content-Length", str(len(page))) 
         self.end_headers()
         self.wfile.write(page)
@@ -33,6 +44,13 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_page(content)
         except IOError as msg:
             msg= "Nije pronadena stranica !"
+    def read_file(self, path):
+        try: 
+            with open(path, "rb") as reader:
+                content = reader.read()
+            return content
+        except IOError as msg:
+            msg = "Nije pronadjena stranica"
 
 if __name__ == '__main__':
     print "Starting web server..."
