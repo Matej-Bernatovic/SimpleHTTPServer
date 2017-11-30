e6fea355f4a2bccf253827ae91f90ab5ffd818bb
Matej <matej.bernatovic@gmail.com>
2017-11-30 11:08:29 +0100

    Add create_page method

--- server.py
+++ server.py
@@ -4,18 +4,29 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     
     Page= """
 <html>
-<body>
-<p>Hello, web!</p>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<body class="container">
+<h1>Hello, web!</h1>
+<div class="container"> This is test server ! </div>
 </body>
 </html>
 """
     #obradi get request
-    def do_GET(self):
+    def do_GET(self):        
+        page = self.create_page()
+        self.send_page(page)
+
+    def create_page(self):
+        page = self.Page
+        return page
+    def send_page(self, page):
         self.send_response(200)
-        self.send_header("Content-Type","text/html")
-        self.send_header("Content-Length", str(len(self.Page)))
+        self.send_header("Content-Type","text/html")       
+        self.send_header("Content-Length", str(len(page))) 
         self.end_headers()
-        self.wfile.write(self.Page)
+        self.wfile.write(page)
 
 if __name__ == '__main__':
     print "Starting web server..."
